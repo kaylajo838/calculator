d85b126c5734671824765d76123cd271597b33c5
kaylajo838 <kaylaimming@gmail.com>
2022-05-21 19:53:42 -0700

    Delete for loop and add a for each loop instead to compress code

--- script.js
+++ script.js
@@ -26,47 +26,13 @@ clear.addEventListener('click', () => {
     display.textContent = zero.value;
 })
 
-// Loop through number buttons and add value to the display
-let index = undefined;
-for (let i = 0; i < numBtns.length; i++) {
-    numBtns[i].addEventListener('click', () => {
-        index = i;
-        switch (index) {
-            case 0:
-                display.textContent = seven.value;
-                break;
-            case 1:
-                display.textContent = eight.value;
-                break;
-            case 2:
-                display.textContent = nine.value;
-                break;
-            case 3:
-                display.textContent = four.value;
-                break;
-            case 4:
-                display.textContent = five.value;
-                break;
-            case 5:
-                display.textContent = six.value;
-                break;
-            case 6:
-                display.textContent = one.value;
-                break;
-            case 7:
-                display.textContent = two.value;
-                break;
-            case 8:
-                display.textContent = three.value;
-                break;
-            case 9:
-                display.textContent = zero.value;
-                break;
-            default:
-                display.textContent = '0';
-        }
+// Loop through number buttons and add it's value to the display
+numBtns.forEach(numBtn => {
+    numBtn.addEventListener('click', () => {
+        display.textContent = numBtn.value;
     })
-}
+})
+
 
 
 function addition() {
